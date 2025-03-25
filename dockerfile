bf941502b06ba916073f6302a4deee1a19f79278
Thomas Caron <thomas.caron.dev@gmail.com>
2025-03-25 14:34:34 +0100

    build: Project preparation with Docker, webpack again for VueJS, Tailwindcss and Flowbite

---
+++ dockerfile
@@ -0,0 +1,10 @@
+FROM php:8.2-fpm
+
+RUN apt-get update && apt-get install -y \
+    libzip-dev \
+    libpq-dev \
+    unzip \
+    git && \
+    docker-php-ext-install zip pdo pdo_pgsql
+
+CMD ["php-fpm"]
---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'app_homepage')]
+class HomepageController extends AbstractController
+{
+    public function __invoke(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
